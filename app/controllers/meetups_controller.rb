6cb40814184907e0ae386c23db6b183eba7bb84b
Uthaeus <romanlavery@gmail.com>
2023-04-28 16:57:19 -0600

    successfully saving uploaded images from frontend

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -46,6 +46,6 @@ class MeetupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def meetup_params
-      params.require(:meetup).permit(:title, :description, :time, :image, :location, :attending, :favorited)
+      params.require(:meetup).permit(:title, :description, :time, :image, :location)
     end
 end
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,7 +1,5 @@
 class Meetup < ApplicationRecord
     validates_presence_of :title, :description, :time, :location
 
-    has_one_attached :image
-
     mount_uploader :image, MeetupUploader
 end
