3cc2d1df4c95668b61e322150faa8467efd228e9
Uthaeus <romanlavery@gmail.com>
2023-04-28 13:40:35 -0600

    created migration to add location to meetups

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,5 +1,5 @@
 class Meetup < ApplicationRecord
-    validates_presence_of :title, :description, :time
+    validates_presence_of :title, :description, :time, :location
 
     has_one_attached :image
 
---
+++ db/migrate/20230428193658_add_location_to_meetup.rb
@@ -0,0 +1,5 @@
+class AddLocationToMeetup < ActiveRecord::Migration[7.0]
+  def change
+    add_column :meetups, :location, :string
+  end
+end
