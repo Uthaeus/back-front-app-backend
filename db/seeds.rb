95f8c52c0ac7d0d9476709cd6cd4202d4d56133c
Uthaeus <romanlavery@gmail.com>
2023-04-28 20:11:41 -0600

    seeded meetup data with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
         title: "Meetup #{meetup}",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         time: "2020-10-#{meetup + 1} 00:00:00",
-        image: "https://placehold.co/600x400",
+        image: File.open(Rails.root.join('public', 'images', 'winter_is_coming.jpg')),
         location: "123 Main St. Anywhere, USA"
     )
 end
